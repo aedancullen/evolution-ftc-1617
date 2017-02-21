db9309d620ba6df5da304f84a47d6b790e3730d2
Aedan Cullen <aedancullen@gmail.com>
2017-02-20 20:16:34 -0500

    Add debugging code to Measurement opmode

--- src/main/java/org/firstinspires/ftc/teamcode/Measurement.java
+++ src/main/java/org/firstinspires/ftc/teamcode/Measurement.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.LightSensor;
+import com.qualcomm.robotcore.hardware.UltrasonicSensor;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -14,7 +16,23 @@ public class Measurement extends OpMode {
 
     List<DcMotor> motors;
 
+    LightSensor lineBopperLeft;
+    LightSensor lineBopperRight;
+
+
+    UltrasonicSensor wallBopperLeft;
+    UltrasonicSensor wallBopperRight;
+
     public void init() {
+
+        lineBopperLeft = hardwareMap.lightSensor.get("liLineL");
+        lineBopperLeft.enableLed(true);
+        lineBopperRight = hardwareMap.lightSensor.get("liLineR");
+        lineBopperRight.enableLed(true);
+
+        wallBopperLeft = hardwareMap.ultrasonicSensor.get("ulWallL");
+        wallBopperRight = hardwareMap.ultrasonicSensor.get("ulWallR");
+
         motors = Arrays.asList(
                 hardwareMap.dcMotor.get("dcOmni0"),
                 hardwareMap.dcMotor.get("dcOmni90"),
@@ -34,6 +52,10 @@ public class Measurement extends OpMode {
     public void loop() {
         for (int i = 0; i < motors.size(); i++) {
             telemetry.addData("Motor " + i + ":", motors.get(i).getCurrentPosition());
+            telemetry.addData("lineBopperLeft", lineBopperLeft.getLightDetected());
+            telemetry.addData("lineBopperRight", lineBopperRight.getLightDetected());
+            telemetry.addData("wallBopperRight", wallBopperRight.getUltrasonicLevel());
+            telemetry.addData("wallBopperLeft", wallBopperLeft.getUltrasonicLevel());
         }
         telemetry.update();
     }
